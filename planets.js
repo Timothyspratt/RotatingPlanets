b1de0e41431944b41e9672d32de50a15c643580c
Tim <timothyspratt88@gmail.com>
2023-05-11 20:18:12 -0500

    anothercommit

--- planets.js
+++ planets.js
@@ -1,12 +1,24 @@
-var css = document.querySelector("h3");
-var color1 = document.querySelector(".color1");
-var color2 = document.querySelector(".color2");
-var body = document.getElementById("gradient");
+// var css = document.querySelector("h3");
+// var color1 = document.querySelector(".color1");
+// var color2 = document.querySelector(".color2");
+// var body = document.getElementById("gradient");
 
-function setGradient() {
-    body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
-}
+// function setGradient() {
+//     body.style.background = "linear-gradient(to right, " + color1.value + "," + color2.value + ")";
+// }
 
-color1.addEventListener("input", setGradient);
+// color1.addEventListener("input", setGradient);
 
-color2.addEventListener("input", setGradient);
+// color2.addEventListener("input", setGradient);
+
+// let secondPlanet = document.querySelector(".second-planet-container");
+
+// secondPlanet.addEventListener("click", sayGoodJob);
+
+document.getElementById("first-planet-index");
+
+function sayGoodJob() {
+    alert("good job!!!!");
+};
+
+console.log(sayGoodJob);
